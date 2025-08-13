dbcf298ace2fd0c38d9b0a465d09a309a44f3137
bj.kim <keinus01@gmail.com>
2025-08-13 22:51:49 +0900

    fix docker build script

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM eclipse-temurin:21.0.7_6-jre-ubi9-minimal
 
 WORKDIR /app
+ARG APP_VERSION
 
-COPY build/libs/logparser-0.2.1.jar /app/logparser-0.2.1.jar
+COPY build/libs/logparser-${APP_VERSION}.jar /app/logparser-v.jar
 
-ENTRYPOINT ["java","-jar","/app/logparser-0.2.1.jar"]
+ENTRYPOINT ["java","-jar","/app/logparser-${APP_VERSION}.jar"]
--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+VERSION=$(grep "^version" build.gradle | head -n 1 | sed -E "s/version\s*=\s*['\"]([^'\"]+)['\"]/\1/")
+echo $VERSION
+
 ./gradlew build
-sudo docker build . --tag=keinus/logparser:latest
+
+sudo docker build . --build-arg APP_VERSION=$VERSION --tag=keinus/logparser:$VERSION
+sudo docker tag keinus/logparser:$VERSION 192.168.50.22:8081/repository/docker/logparser:$VERSION
+sudo docker push 192.168.50.22:8081/repository/docker/logparser:$VERSION
+sudo docker push 192.168.50.22:8081/repository/docker/logparser:latest
